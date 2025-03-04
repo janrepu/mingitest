ab832b55e27227213fb59f3e7344f2f137c41de8
janrepu <janre.puuorg@voco.ee>
2025-03-04 11:12:50 +0200

    B osa

--- welcome.py
+++ welcome.py
@@ -1,4 +1,16 @@
+<<<<<<< feature-b
 from validate_age import *
 
 age = int(input("Enter your age: "))
 validate_age(age)
+======
+from get_name import *
+
+print("="*35)
+print("Welcome to voting eligiblity check")
+print("="*35)
+print("\n)
+
+user_name = get_name()
+print(f"Hello {user_name}!\n")
+>>>>>>> develop
